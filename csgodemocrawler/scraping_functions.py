24bf02314cfeb4c37c9607ba27f1ba2d2c04765c
Erick Rocha <erickkrocha@gmail.com>
2019-12-28 22:53:20 -0300

    Data :: scraping a bunch of other important infos
    - Removed 'raw_draft' field from exported json
    - Scraping more fields:
      - match_id
      - match_date
      - maps - with info pertaining each map played and its results

---
+++ csgodemocrawler/scraping_functions.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+
+''' Event Results Page XPATH accessors '''
+
+# The event name
+get_event_name = lambda resp: resp\
+    .xpath('//div[contains(@class, "eventname")]/text()')\
+    .get()
+
+# The location of the event
+get_location = lambda resp: resp\
+    .xpath('//td[contains(@class, "location")]//span/text()')\
+    .get()
+
+# The amount of team participating on the champtionship
+get_teams_amount = lambda resp: resp\
+    .xpath('//td[@class="teamsNumber"]/text()')\
+    .get()
+
+# The prizepool of the championship
+get_prizepool = lambda resp: resp\
+    .xpath('//td[contains(@class, "prizepool")]/text()')\
+    .get()
+
+# The championship date span
+get_date_span = lambda resp: resp\
+    .xpath('//td[@class="eventdate"]//span/text()')\
+    .extract()
+
+# The match URLs
+get_match_urls = lambda resp: resp\
+    .xpath('//div[contains(@class, "result-con")]//a/@href')\
+    .extract()
+
+
+''' Match Page XPATH accessors '''
+
+# The GOTV-demo URL
+get_gotv_demo_url = lambda resp: resp\
+    .xpath('//div[contains(@class, "streams")]//a[contains(@href, "demo")]/@href')\
+    .get()
+
+# The match date
+get_match_date = lambda resp: resp\
+    .xpath('//div[@class="timeAndEvent"]/div[@class="date"]/text()')\
+    .get()
+
+# The team names
+get_team_names = lambda resp: resp\
+    .xpath('//div[@class="match-page"]//div[@class="team"]//div[@class="teamName"]//text()')\
+    .extract()
+
+# The match format (e.g. bo2, bo3..)
+get_match_format = lambda resp: resp\
+    .xpath('//div[@class="match-page"]//div[contains(@class, "veto-box")][1]//div/text()')\
+    .get()
+
+# The raw draft information
+get_raw_draft_info = lambda resp: resp\
+    .xpath('//div[@class="match-page"]//div[contains(@class, "veto-box")][2]//div/text()')\
+    .extract()
+
+# The maps played in this match
+get_played_maps_containers = lambda resp: resp\
+    .xpath('//div[@class="mapholder"]')
+
+# The name of the played map (within supplied container)
+get_map_name = lambda container: container\
+    .xpath('./div[contains(@class, "played")]//div[contains(@class, "mapname")]/text()')\
+    .get()
+
+# The results fo the map (within supplied container)
+get_map_results_container = lambda container: container\
+    .xpath('./div[@class="results"]//*[contains(@class, "results-teamname-container")]')
+
+# The team name to whom this result pertains to
+get_map_result_teamname = lambda container: container\
+    .xpath('./div[contains(@class, "results-teamname")]/text()')\
+    .get()
+
+# The resultant score of the map
+get_map_result_score = lambda container: container\
+    .xpath('./div[contains(@class, "results-team-score")]/text()')\
+    .get()
+
+''' Scraping sub-routines '''
+
+def get_draft_info(response, teams):
+    ''' Parses and structures the scraped draft info '''
+    raw_draft = get_raw_draft_info(response)
+    raw_draft = [draft_step for draft_step in raw_draft if draft_step.strip() != '']
+
+    team_1 = teams[0]
+    team_2 = teams[1]
+
+    draft = []
+    for choice in raw_draft:
+        if choice.find(team_1) < 0 and choice.find(team_2) < 0:
+            # default pick
+            # TODO: parse default cases
+            continue
+
+        if choice.find(team_1) >= 0:
+            # 1st team's choice
+            team = team_1[:]
+            choice = choice.replace(team, '').strip()
+
+        elif choice.find(team_2) >= 0:
+            # 2nd team's choice
+            team = team_2[:]
+            choice = choice.replace(team, '').strip()
+
+        tokens = choice.split(' ')
+
+        action = tokens[2].strip()
+        chosen_map = tokens[3].strip()
+
+        draft.append({
+            'team': team,
+            'action': action,
+            'map': chosen_map
+        })
+
+    return draft
+
+def get_played_maps_info(response):
+    ''' Parses map info from its scraped HTML container '''
+    # getting played maps HTML containers
+    played_maps_containers = get_played_maps_containers(response)
+
+    played_maps = []
+
+    # for each played map container, we look for the desired info:
+    for idx, map_container in enumerate(played_maps_containers):
+        # scraping map name
+        map_name = get_map_name(map_container)
+
+        if not map_name:
+            # probably an unplayed map
+            #   (match decided in the previous maps)
+            continue
+
+        map_info = {
+            'map': map_name,
+            'scores': []
+        }
+
+        # getting results container
+        results_container = get_map_results_container(map_container)
+
+        # scraping each team name & score
+        for result_container in results_container:
+            team_name = get_map_result_teamname(result_container)
+            score = get_map_result_score(result_container)
+
+            map_info['scores'].append({
+                'team': team_name,
+                'score': score
+            })
+
+        played_maps.append(map_info)
+
+    return played_maps
+        
--- csgodemocrawler/spiders/eventresults.py
+++ csgodemocrawler/spiders/eventresults.py
@@ -3,6 +3,8 @@ import json
 import re
 import scrapy
 
+from .. import scraping_functions
+
 
 class EventResultsSpider(scrapy.Spider):
     name = 'eventresults'
@@ -27,12 +29,12 @@ class EventResultsSpider(scrapy.Spider):
     def scrape_event_page(self, response):
         ''' Scrapes event page for match URLs yields them as they're found '''
         # scraping event metadata
-        event_name = response.xpath('//div[contains(@class, "eventname")]/text()').get()
-        location = response.xpath('//td[contains(@class, "location")]//span/text()').get()
-        teams_amount = response.xpath('//td[@class="teamsNumber"]/text()').get()
-        prizepool = response.xpath('//td[contains(@class, "prizepool")]/text()').get()
+        event_name = scraping_functions.get_event_name(response)
+        location = scraping_functions.get_location(response)
+        teams_amount = scraping_functions.get_teams_amount(response)
+        prizepool = scraping_functions.get_prizepool(response)
 
-        dates = response.xpath('//td[@class="eventdate"]//span/text()').extract()
+        dates = scraping_functions.get_date_span(response)
         # formatting date interval
         date_span = ' '.join(date.strip() for date in dates)
 
@@ -51,89 +53,67 @@ class EventResultsSpider(scrapy.Spider):
         self.event_manifest['date'] = date_span
 
         # scraping event match URLs & yielding them to next step in pipeline
-        for rel_match_url in response.xpath('//div[contains(@class, "result-con")]//a/@href').extract():
+        for rel_match_url in scraping_functions.get_match_urls(response):
             abs_match_url = response.urljoin(rel_match_url)
             yield scrapy.Request(abs_match_url, self.scrape_match_page)
 
 
     def scrape_match_page(self, response):
         ''' Scrapes match page for relevant metadata info and for the GOTV-demo download URL '''
-        rel_gotv_demo_url = response\
-            .xpath('//div[contains(@class, "streams")]//a[contains(@href, "demo")]/@href')\
-            .get()
+        # scraping gotv demo url
+        rel_gotv_demo_url = scraping_functions.get_gotv_demo_url(response)
 
-        if not rel_gotv_demo_url:
+        # skipping this page if no demo is available
+        if not rel_gotv_demo_url: 
             return
-
         abs_gotv_demo_url = response.urljoin(rel_gotv_demo_url)
 
-        team_names = response\
-            .xpath('//div[contains(@class, "teamsBox")]//div[@class="team"]//div[@class="teamName"]//text()')\
-            .extract()
+        # extract match id from URL
+        m = re.search(r'\/matches\/([0-9]+)\/', response.url)
+        match_id = m.group(1)
+
+        # scraping team names
+        team_names = scraping_functions.get_team_names(response)
 
         team_1 = team_names[0]
         team_2 = team_names[1]
 
-        match_format_raw = response\
-            .xpath('///div[@class="match-page"]//div[contains(@class, "veto-box")][1]//div/text()')\
-            .get()
+        match_format_raw = scraping_functions.get_match_format(response)
 
+        # scraping match format {bo1, bo2, ..}
         if match_format_raw:
             m = re.match(r'^Best of ([0-9]{1})', match_format_raw.strip())
             match_format = m.group(1)
 
+        # if the match format was bo2 or higher,
+        #   there was a draft to select the maps
         if match_format and int(match_format) > 1:
-            raw_draft = response\
-                .xpath('//div[@class="match-page"]//div[contains(@class, "veto-box")][2]//div/text()')\
-                .extract()
+            draft = scraping_functions.get_draft_info(response, team_names)
 
-            raw_draft = [draft_step for draft_step in raw_draft if draft_step.strip() != '']
-            draft = []
-
-            for choice in raw_draft:
-                if choice.find(team_1) < 0 and choice.find(team_2) < 0:
-                    # default pick
-                    continue
-
-                if choice.find(team_1) >= 0:
-                    # 1st team's choice
-                    team = team_1[:]
-                    choice = choice.replace(team, '').strip()
-
-                elif choice.find(team_2) >= 0:
-                    # 2nd team's choice
-                    team = team_2[:]
-                    choice = choice.replace(team, '').strip()
-
-                tokens = choice.split(' ')
-
-                action = tokens[2].strip()
-                chosen_map = tokens[3].strip()
-
-                draft.append({
-                    'team': team,
-                    'action': action,
-                    'map': chosen_map
-                })
+        # scraping results of maps played in this match
+        maps_played = scraping_functions.get_played_maps_info(response)
 
         print()
         print(f'Match URL :: {response.url}')
         print(f'Teams :: {team_1} VS {team_2}')
         print(f'Match Format :: bo{str(match_format)}')
         print(json.dumps(draft, indent=4))
+        print(json.dumps(maps_played, indent=4))
         print()
 
         # initializing match manifest with competing teams & GOTV-demo URL
         match_info = {
-            'teams': team_names,
+            'match_id': match_id,
+            'match_date': scraping_functions.get_match_date(response),
             'match_url': response.url,
-            'gotv_demo_url': abs_gotv_demo_url
+            'gotv_demo_url': abs_gotv_demo_url,
+            'teams': team_names,
         }
 
         # saving draft info - if available
-        if raw_draft:
-            match_info['draft'] = raw_draft
-            match_info['parsed_draft'] = draft
+        if draft: match_info['draft'] = draft
+
+        match_info['maps'] = maps_played
 
         if 'matches' not in self.event_manifest:
             self.event_manifest['matches'] = []
